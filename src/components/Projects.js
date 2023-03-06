d7bf3c88bc2a7503ed7f399e5ce3af31e10415f1
IzukiKa <isaacjroberts0505@gmail.com>
2023-03-06 22:23:33 +0000

    ➕ Update Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,22 +8,26 @@ import projImg3 from "../assets/img/project-img3.png";
 import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-    const projects = [
-        {
-            title: "Mystcraft: Ages",
-            description: "Minecraft mod based on the game franchise \"Myst\".",
-            imgUrl: projImg1
-        },
+    const datapacks = [
         {
             title: "Voidborn",
             description: "Post-End Minecraft datapack based around the void.",
             imgUrl: projImg2
-        },
+        }
+    ];
+    const mods = [
+        {
+            title: "Mystcraft: Ages",
+            description: "Minecraft mod based on the game franchise \"Myst\".",
+            imgUrl: projImg1
+        }
+    ];
+    const other = [
         {
             title: "Command Database",
             description: "A database and guide for all things Minecraft command and function based!",
             imgUrl: projImg3
-        },
+        }
     ];
 
     return (
@@ -39,20 +43,20 @@ export const Projects = () => {
                                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                         <Nav.Item>
-                                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                            <Nav.Link eventKey="first">Datapacks</Nav.Link>
                                         </Nav.Item>
                                         <Nav.Item>
-                                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                            <Nav.Link eventKey="second">Mods</Nav.Link>
                                         </Nav.Item>
                                         <Nav.Item>
-                                            <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                            <Nav.Link eventKey="third">Other</Nav.Link>
                                         </Nav.Item>
                                     </Nav>
                                     <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__salideInUp" : ""}>
                                         <Tab.Pane eventKey="first">
                                             <Row>
                                                 {
-                                                    projects.map((project, index) => {
+                                                    datapacks.map((project, index) => {
                                                         return (
                                                             <ProjectCard 
                                                             key={index}
@@ -66,10 +70,36 @@ export const Projects = () => {
                                             </Row>
                                         </Tab.Pane>
                                         <Tab.Pane eventKey="second">
-                                            Second
+                                            <Row>
+                                                {
+                                                    mods.map((project, index) => {
+                                                        return (
+                                                            <ProjectCard 
+                                                            key={index}
+                                                            title={project.title}
+                                                            description={project.description}
+                                                            imageUrl={project.imgUrl}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
                                         </Tab.Pane>
                                         <Tab.Pane eventKey="third">
-                                            Third
+                                            <Row>
+                                                {
+                                                    other.map((project, index) => {
+                                                        return (
+                                                            <ProjectCard 
+                                                            key={index}
+                                                            title={project.title}
+                                                            description={project.description}
+                                                            imageUrl={project.imgUrl}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
                                         </Tab.Pane>
                                     </Tab.Content>
                                 </Tab.Container>
